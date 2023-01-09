12833f657d0dd77b068347842ebd52211b11e660
Jesus Alexis <ingjesusalexis@gmail.com>
2023-01-08 20:44:03 -0400

    added insert db

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -5,10 +5,11 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Controller('pokemon')
 export class PokemonController {
-  constructor(private readonly pokemonService: PokemonService) {}
+  constructor(private readonly pokemonService: PokemonService) { }
 
   @Post()
   create(@Body() createPokemonDto: CreatePokemonDto) {
+
     return this.pokemonService.create(createPokemonDto);
   }
 
